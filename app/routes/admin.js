72c1eed3e5039e211b140dceac1da6b3ed4b4786
Bruno Ricci <brunoriccidossantos@hotmail.com>
2017-09-03 14:42:05 -0300

    Ajustes na implementação do express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,7 +6,8 @@ module.exports = function(app){
 
 	//Ouve chamada pelo método GET e carrega o formulário de inclusão de notícias
 	app.get('/formulario_inclusao_noticia', function(req, res){
-		res.render('admin/form_add_noticia', {validacao: {}});
+		//Envia-se as variáveis de validação e erro vazias para não causar erro na renderização inicial
+		res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
 		res.render('admin/form_add_noticia');
 	});
 
@@ -19,22 +20,23 @@ module.exports = function(app){
 		req.assert('resumo','O resumo deve conter entre 10 e 100 caracteres').len(10,100);
 		req.assert('autor','O nome do autor é obrigatório').notEmpty();
 		req.assert('autor','O nome do autor não deve ter mais que 30 caracteres').len(1,30);
-		req.assert('data_noticia','Data inválida').notEmpty();
 //		isDate pode esta depreciado, somente funciona com a versão do express-validator definida para ^3.2.1
-		req.assert('data_noticia','Data inválida').isDate({format:'YYYY-MM-DD'});
+		req.assert('data_noticia','Data inválida').notEmpty().isDate({format:'YYYY-MM-DD'});
 		req.assert('autor','A notícia é obrigatória').notEmpty();
 
 		var erros = req.validationErrors();
 
+		//Se a variável erros for definida(quando há erros), renderizar o formulário informando os erros
 		if( erros ){
-			res.render('admin/form_add_noticia', {validacao: erros});
+			//Envia-se a notícia para o formulário para mante-lo preenchido para a correção
+			res.render('admin/form_add_noticia', {validacao: erros, noticia: noticia});
 			return;
 		}
 
 		var noticiasModel = new app.app.models.NoticiasDAO(
 				app.config.dbConnection());
 
-		//Sanvando informação
+		//Salvando informação
 		noticiasModel.salvarNoticia(noticia, function(erro, result){
 			//Retorna status 302 redirecionando para /noticias
 			res.redirect('/noticias');
