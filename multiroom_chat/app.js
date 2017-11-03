dc7916bfd0686449692a8d736b7f0613fc9afe11
Bruno Ricci dos Santos <brunoriccidossantos@hotmail.com>
2017-11-03 17:23:20 -0200

    Implementação básica do Socket.io

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -2,6 +2,17 @@
 var app = require('./config/server');
 
 /* parametrizar porta de escuta */
-app.listen(app.config.serverPort, function(){
+var server = app.listen(app.config.serverPort, function(){
     console.log('Servidor online ouvindo a porta ' + app.config.serverPort);
 })
+
+var io = require('socket.io').listen(server);
+
+/* 	Criando conexão via websocket */
+io.on('connection',function(socket){
+	console.log('O usuário conectou-se');
+
+	io.on('disconnect',function(socket){
+		console.log('O usuário desconectou-se');
+	});
+});
