95da88b371e050fb61c90da3eb7ba80a384004a6
Bruno Ricci dos Santos <brunoriccidossantos@hotmail.com>
2017-12-14 17:54:26 -0200

    Inserindo módulo do express-session

--- mmorpg_got_arquivos_do_projeto/app/controllers/index.js
+++ mmorpg_got_arquivos_do_projeto/app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(app,req,res){
+	res.render('index',{validacao:{}});
+}
+
+module.exports.autenticar = function(app,req,res){
+
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Você deve preencher o campo usuário').notEmpty();
+	req.assert('senha', 'Você deve preencher o campo senha').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index',{validacao:erros});
+		return;
+	}
+
 	res.render('index');
 }
