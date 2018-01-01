84b17d69851d5d89d12dda619503c3e3df178a3d
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-01-01 19:21:48 -0200

    Rollback na branch

--- mmorpg_got_arquivos_do_projeto/app.js
+++
@@ -1,7 +0,0 @@
-/* importar as configurações do servidor */
-var app = require('./config/server');
-
-/* parametrizar a porta de escuta */
-app.listen(app.config.serverPort, function(){
-	console.log('Servidor online');
-})
